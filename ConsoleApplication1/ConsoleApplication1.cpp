235d1fa47fab11a92927e314739450490bcbb980
Kutsenko Vladislav <81022091+Desters@users.noreply.github.com>
2024-10-30 19:46:13 +0200

    First push
    This is my first push.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <Windows.h>
+
+
+
+int main() {
+    srand(time(0));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Вітаю!" << std::endl;
+}
